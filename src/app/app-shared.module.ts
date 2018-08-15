b499f0b26dd1b091d622b9ce90e17eef56d3c05e
stephenautomatik <sdickens@automatik.us>
2018-08-14 20:49:34 -0700

    upgrade to angular-fontawesome

--- src/app/app-shared.module.ts
+++ src/app/app-shared.module.ts
@@ -16,12 +16,46 @@ import {
   MatTabsModule
 } from '@angular/material';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faCaretDown,
+  faEdit,
+  faEye,
+  faPlus,
+  faSearch,
+  faTimes,
+  faTrashAlt
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebookF,
+  faGooglePlusG,
+  faInstagram,
+  faLinkedinIn,
+  faPinterest,
+  faTwitter
+} from '@fortawesome/free-brands-svg-icons';
+
 import { CookieModule } from 'ngx-cookie';
 
+// Directives
 import { MaxValidatorDirective } from './directives/max-validator.directive';
 import { MinValidatorDirective } from './directives/min-validator.directive';
 
-// Directives
+// Icons for fontawesome library
+library.add(faCaretDown);
+library.add(faEdit);
+library.add(faEye);
+library.add(faPlus);
+library.add(faSearch);
+library.add(faTimes);
+library.add(faTrashAlt);
+library.add(faFacebookF);
+library.add(faGooglePlusG);
+library.add(faInstagram);
+library.add(faLinkedinIn);
+library.add(faPinterest);
+library.add(faTwitter);
 
 @NgModule({
   imports: [
@@ -39,6 +73,7 @@ import { MinValidatorDirective } from './directives/min-validator.directive';
     MatSortModule,
     MatTableModule,
     MatTabsModule,
+    FontAwesomeModule,
     CookieModule.forRoot()
   ],
   declarations: [
@@ -60,6 +95,7 @@ import { MinValidatorDirective } from './directives/min-validator.directive';
     MatSortModule,
     MatTableModule,
     MatTabsModule,
+    FontAwesomeModule,
     CookieModule,
     MaxValidatorDirective,
     MinValidatorDirective
